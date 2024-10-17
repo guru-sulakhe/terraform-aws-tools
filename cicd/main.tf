2e4e72af56b5d961030daae684c6de7c8929747f
guru-sulakhe <sulakheguruprasad@gmail.com>
2024-10-17 10:48:05 +0530

    nexus

--- cicd/main.tf
+++ cicd/main.tf
@@ -29,16 +29,31 @@ module "jenkins_agent" {
   }
 }
 
+resource "aws_key_pair" "tools" {
+  key_name   = "tools"
+  # you can paste the public key directly like this
+  #public_key = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIL6ONJth+DzeXbU3oGATxjVmoRjPepdl7sBuPzzQT2Nc sivak@BOOK-I6CR3LQ85Q"
+  public_key = file("~/.ssh/tools.pub")
+  # ~ means windows home directory
+}
+
 module "nexus" {
   source  = "terraform-aws-modules/ec2-instance/aws"
 
   name = "nexus"
 
-  instance_type          = "t3.small"
+  instance_type          = "t3.medium"
   vpc_security_group_ids = ["sg-0ae3a12e7b7696e53"]
   # convert StringList to list and get first element
   subnet_id = "subnet-0be035a5a62ef124e"
   ami = data.aws_ami.nexus_ami_info.id
+  key_name = aws_key_pair.tools.key_name
+  root_block_device = [
+    {
+      volume_type = "gp3"
+      volume_size = 30
+    }
+  ]
   tags = {
     Name = "nexus"
   }
