962a2b52151ccd7bc494660e81b1a5b4cfa96a59
guru-sulakhe <163813748+guru-sulakhe@users.noreply.github.com>
2025-06-02 21:02:40 +0530

    Update jenkins.sh

--- cicd/jenkins.sh
+++ cicd/jenkins.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+#Installing jenkins dependencies
 curl -o /etc/yum.repos.d/jenkins.repo https://pkg.jenkins.io/redhat-stable/jenkins.repo
 rpm --import https://pkg.jenkins.io/redhat-stable/jenkins.io-2023.key
 yum install fontconfig java-17-openjdk jenkins -y
